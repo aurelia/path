9342f179e548847f6c27d7e8a5b7fbb275f9c5b2
Rob Eisenberg <rob@bluespire.com>
2014-12-16 15:52:34 -0500

    feat(join): add a utility for joining paths
    This commit adds a utility function for joining two paths together. It
    differentiates itself from the normalize function because normalize
    produces a path relative to a base file location.
    
    This commit also adds an acknowledgements document recognizing the
    source of the various bathing algorithms.

--- test/path.spec.js
+++ test/path.spec.js
@@ -1,10 +1,19 @@
-import { normalize } from '../lib/index';
+import { normalize, join } from '../src/index';
 
 describe('normalize', () => {
-  it('can normalize a path with a baseUrl', () => {
-    var baseUrl = 'http://durandal.io/';
-    var path = './my/module';
+  it('can make a path relative to a file', () => {
+    var baseUrl = 'some/file.html';
+    var path = './other/module';
 
-    expect(normalize(path, baseUrl)).toBe('http://durandal.io/my/module');
+    expect(normalize(path, baseUrl)).toBe('some/other/module');
+  });
+});
+
+describe('join', () => {
+  it('can join two paths', () => {
+    var path1 = 'one';
+    var path2 = 'two';
+
+    expect(join(path1, path2)).toBe('one/two');
   });
 });
